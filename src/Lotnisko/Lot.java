73b2574d2e912ca7fd1545e660d635da9ff0448a
Mateusz Wilczewski <wilczewski.2407@gmail.com>
2022-05-27 14:59:31 +0200

    in ps

--- src/Lotnisko/Lot.java
+++ src/Lotnisko/Lot.java
@@ -1,11 +1,27 @@
 package Lotnisko;
 
 import java.util.Calendar;
+import java.util.Date;
+
+
 
 public class Lot {
     Trasa trasa;
-    Samolot s;
-    Calendar data;
-    Calendar czas;
+    Samolot samolot;
+    Calendar data,przylot;
     int numer_lotu;
+    Bilet[] bilety;
+    Lot(Trasa t, Samolot s, Calendar data){
+        trasa=t;
+        samolot=s;
+        numer_lotu=this.hashCode();
+        Bilety=new Bilet[s.getLiczbaMiejsc];
+        przylot= Calendar.getInstance();
+        przylot.setTime(data.getTime());
+        przylot.add(Calendar.HOUR,trasa.getOdleglosc*10);
+    }
+    Lot(Lot l,Calendar){
+
+    }
+
 }
