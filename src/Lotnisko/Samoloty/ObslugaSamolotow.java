fc01b28994d075a0a0295a771319b0fab1356453
wilczany <wilczewski.2407@gmail.com>
2022-06-08 02:07:29 +0200

    dodawanie lotnisk w aplikacji okienkowej

---
+++ src/Lotnisko/Samoloty/ObslugaSamolotow.java
@@ -0,0 +1,16 @@
+package samoloty;
+
+import main.Controller;
+
+import java.util.ArrayList;
+
+public class ObslugaSamolotow {
+        ArrayList<Samolot> flota=new ArrayList<>();
+
+
+
+        public static class SamlotyController extends Controller{
+
+
+        }
+        }
--- src/Lotnisko/module-info.java
+++ src/Lotnisko/module-info.java
@@ -6,5 +6,11 @@ module IdeaProjects {
     exports main;
     opens trasy to javafx.fxml;
     exports trasy;
+    opens loty to javafx.fxml;
+    exports loty;
+    opens samoloty to javafx.fxml;
+    exports samoloty;
+    opens uslugobiorcy to javafx.fxml;
+    exports uslugobiorcy;
 
 }
