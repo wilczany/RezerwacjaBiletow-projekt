0c71d1e82cf290c77c5ddd82a931b0e3adbc850f
papryku <supteros@gmail.com>
2022-06-08 22:03:15 +0200

    eeee ee
    samolot lekki dodatek ma

--- src/Lotnisko/samoloty/ObslugaSamolotow.java
+++ src/Lotnisko/samoloty/ObslugaSamolotow.java
@@ -10,6 +10,29 @@ public class ObslugaSamolotow extends Controller{
 
 
         //Kontroler
+        //ArrayList<Lot> loty; jednak UML troszke przestarzaly xDD
+        ArrayList<Samolot> samoloty;
 
-
+        //exception specjalne pod takie same id? sama informacja w okienku tekstowym?
+        public void dodajSamolot(Samolot s){
+                for(Samolot sm : samoloty){
+                        if(s.equals(sm)) return;
+                }
+                samoloty.add(s);
         }
+
+        public void usunSamolot(String ID){
+                int i=0, j=-1;
+                for(Samolot sm : samoloty){
+                        if(sm.getID().equals(ID)) j=i;
+                        i++;
+                }
+                if(j!=-1) samoloty.remove(j);
+                else return;
+                /*
+                for(Lot l : loty){
+                        if(l.getSamolot().getID().equals(ID)) ObslugaLotow.anulujLot(tamten lot)
+                }
+                */
+        }
+}
