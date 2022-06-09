d8ec3f3575402b53070b2223477320d5a2e3d9e8
Maraxoni <maraxoni16@gmail.com>
2022-06-09 21:06:11 +0200

    Dodawanie test
    Leb mnie boli ide spac

--- src/Lotnisko/uslugobiorcy/KlienciController.java
+++ src/Lotnisko/uslugobiorcy/KlienciController.java
@@ -6,6 +6,42 @@ import main.Controller;
 
 public class KlienciController extends Controller {
 
+    @FXML
+    public void dodajLotnisko(ActionEvent event) {
+        Dialog<String> dialog = new TextInputDialog();
+        dialog.setHeaderText("Podaj imie/NIP:");
+        dialog.setContentText("Imie/NIP: ");
+        Optional<String> result_nazwa = dialog.showAndWait();
+        String nazwa = result_nazwa.get();
+        if(NaszaFirma.getInstance().obslugaTras.sprawdzNazwe(nazwa) || nazwa==""){
+            dialog = new Dialog<String>();
+            dialog.setTitle("Błąd");
+            ButtonType bOk = new ButtonType("OK", ButtonData.OK_DONE);
+            dialog.setContentText("Podana nazwa jest już zajęta.");
+            dialog.getDialogPane().getButtonTypes().add(bOk);
+            dialog.showAndWait();
+            return;
+        }
+        Dialog<String> dialog = new TextInputDialog();
+        dialog.setHeaderText("Podaj nazwę/nazwisko:");
+        dialog.setContentText("Nazwa/Nazwisko: ");
+        Optional<String> result_nazwa = dialog.showAndWait();
+        String nazwa2 = result_nazwa.get();
+        if(NaszaFirma.getInstance().obslugaTras.sprawdzNazwe(nazwa2) || nazwa2==""){
+            dialog = new Dialog<String>();
+            dialog.setTitle("Błąd");
+            ButtonType bOk = new ButtonType("OK", ButtonData.OK_DONE);
+            dialog.setContentText("Podana nazwa jest już zajęta.");
+            dialog.getDialogPane().getButtonTypes().add(bOk);
+            dialog.showAndWait();
+            return;
+        }
+        
+
+        Klient kkk = new Klient(nazwa,nazwa2);
+        NaszaFirma.getInstance().obslugaKlientow.getKlienci().add(kkk);
+        refresh();
+    }
 
     public void refresh(){
 
