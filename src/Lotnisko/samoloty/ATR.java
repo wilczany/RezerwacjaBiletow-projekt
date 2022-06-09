00b3dacc2e10c011ec66d68be4803f4ad1cdfd1d
wilczany <wilczewski.2407@gmail.com>
2022-06-09 02:19:59 +0200

    predkosc usunieta

--- src/Lotnisko/samoloty/ATR.java
+++ src/Lotnisko/samoloty/ATR.java
@@ -1,15 +1,12 @@
 package samoloty;
 
 public class ATR extends Samolot {
-    
-    protected
-    int Miejsca = 120, Zasieg = 1200, Predkosc = 60;
+
 
     protected ATR(String id) {
         super(id);
-        super.Miejsca = Miejsca;
-        super.Zasieg = Zasieg;
-        super.Predkosc = Predkosc;
+        Miejsca = 120;
+        Zasieg = 1200;
     }
 
 }
